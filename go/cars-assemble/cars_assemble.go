daa3595027d4b34b4ff10c861bef0ec730557b91
Gabriela <gabrielamdesalvo@gmail.com>
2023-08-18 12:01:27 -0300

    Create repo

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,27 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+
+    return float64(productionRate) / successRate
+    
+	panic("CalculateWorkingCarsPerHour not implemented")
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+
+    intSucessRate := int(successRate)
+
+     return int(productionRate / intSucessRate)
+    
+    
+	panic("CalculateWorkingCarsPerMinute not implemented")
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	panic("CalculateCost not implemented")
+}
